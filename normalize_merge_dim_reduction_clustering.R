f0d84412c99fd32225aea927fcd449e6c3e1a69f
jieunesther <94187656+jieunesther@users.noreply.github.com>
2024-04-09 13:38:00 -0400

    Update normalize_merge_dim_reduction_clustering.R

--- normalize_merge_dim_reduction_clustering.R
+++ normalize_merge_dim_reduction_clustering.R
@@ -12,41 +12,40 @@ library(farver)
 library(usethis)
 library(labeling)
 library(Matrix)
-library(stringdist, lib="/proj/zylkalab/Esther/rpackages/")
+library(stringdist)
 
-#CC_combined = readRDS("/proj/zylkalab/Esther/splitseq/Rmd_files/CC_splitseq/output/seurat/alevin-fry_combined_filtered_220325.rds")
-#CC_combined <- PercentageFeatureSet(object = CC_combined, pattern = "^mt-", col.name = "percent.mt")
-#combined.filtered <- subset(CC_combined, subset = nCount_RNA > 1000 & nCount_RNA < 100000 & nFeature_RNA > 500 & nFeature_RNA < 10000 & percent.mt < 10)
-#combined.filtered
+CC_combined = readRDS("/proj/zylkalab/Esther/splitseq/Rmd_files/CC_splitseq/output/seurat/alevin-fry_combined_filtered_220325.rds")
+CC_combined <- PercentageFeatureSet(object = CC_combined, pattern = "^mt-", col.name = "percent.mt")
+combined.filtered <- subset(CC_combined, subset = nCount_RNA > 1000 & nCount_RNA < 100000 & nFeature_RNA > 500 & nFeature_RNA < 10000 & percent.mt < 10)
 
 # Gene-level filtering (remove genes that have zero expression in all cells, then keep only genes which are expressed in 30 or more cells)
 # remove genes not expressed in at least 30 cells, if less than 3 transcripts (have not done this)
 # Output a logical vector for every gene on whether the more than zero counts per cell
 # Extract counts
-#counts <- GetAssayData(object = combined.filtered, slot = "counts")
+counts <- GetAssayData(object = combined.filtered, slot = "counts")
 
 # Output a logical vector for every gene on whether the more than zero counts per cell
-#nonzero <- counts > 0
+nonzero <- counts > 0
 
 # Sums all TRUE values and returns TRUE if more than 30 TRUE values per gene
-#keep_genes <- Matrix::rowSums(nonzero) >= 30
+keep_genes <- Matrix::rowSums(nonzero) >= 30
 
 # Only keeping those genes expressed in more than 30 cells
-#filtered_counts <- counts[keep_genes, ]
+filtered_counts <- counts[keep_genes, ]
 
 # Reassign to filtered Seurat object
-#combined.filtered <- CreateSeuratObject(filtered_counts, meta.data = combined.filtered@meta.data)
+combined.filtered <- CreateSeuratObject(filtered_counts, meta.data = combined.filtered@meta.data)
 
 #options(future.globals.maxSize=5242880000)
-#all.list <- SplitObject(combined.filtered, split.by = "sampleNames")
+all.list <- SplitObject(combined.filtered, split.by = "sampleNames")
 
 #for (i in 1:length(x = all.list)) {
-#  all.list[[i]] <- PercentageFeatureSet(object = all.list[[i]], pattern = "^mt-", col.name = "percent.mt")
-#  all.list[[i]] <- SCTransform(all.list[[i]], vars.to.regress = c("percent.mt"), verbose = FALSE, return.only.var.genes=FALSE)
-#}
+  all.list[[i]] <- PercentageFeatureSet(object = all.list[[i]], pattern = "^mt-", col.name = "percent.mt")
+  all.list[[i]] <- SCTransform(all.list[[i]], vars.to.regress = c("percent.mt"), verbose = FALSE, return.only.var.genes=FALSE)
+}
 
-#saveRDS(all.list, "/proj/zylkalab/Esther/splitseq/Rmd_files/CC_splitseq/output/seurat/alevin-fry_combined_filtered_220325_SCTransform.rds")
-#rm(list=ls())
+saveRDS(all.list, "/proj/zylkalab/Esther/splitseq/Rmd_files/CC_splitseq/output/seurat/alevin-fry_combined_filtered_220325_SCTransform.rds")
+rm(list=ls())
 
 all.list = readRDS("/proj/zylkalab/Esther/splitseq/Rmd_files/CC_splitseq/output/seurat/alevin-fry_combined_filtered_220325_SCTransform.rds")
 combined = merge(all.list[[1]], y = c(all.list[[2]], all.list[[3]], all.list[[4]], all.list[[5]], 
